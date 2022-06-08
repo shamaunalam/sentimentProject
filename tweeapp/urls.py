0d6462d2ef484f1ff72b87a89781d809d876927a
shamaunalam <shamaunalam@gmail.com>
2022-06-08 11:15:44 +0530

    added tweet search button

--- tweeapp/urls.py
+++ tweeapp/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.index,name='index'),
-    path('gettweet/<str:query>',views.get_tweets)
+    path('gettweet',views.get_tweets,name='tweets')
 ]
--- tweeapp/views.py
+++ tweeapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import JsonResponse
 from . import analyze
 # Create your views here.
@@ -17,16 +17,19 @@ def index(request):
         return render(request,'index.html')
 
 
-def get_tweets(request,query):
+def get_tweets(request):
 
-    tweet_fields = "tweet.fields=text"
-    
-    #twitter api call
-    json_response = search_twitter(query=query, tweet_fields=tweet_fields, bearer_token=BEARER_TOKEN)
+    if request.method=='POST':
+        tweet_fields = "tweet.fields=text"
+        query = request.POST['query']
+        #twitter api call
+        json_response = search_twitter(query=query, tweet_fields=tweet_fields, bearer_token=BEARER_TOKEN)
 
-    content = dict()
-    for i,t in enumerate(json_response['data']):
-        content.update({i:{'tweet':t['text'],'sent':analyze.get_sentiment(t['text'])}})
+        content = dict()
+        for i,t in enumerate(json_response['data']):
+            content.update({i:{'tweet':t['text'],'sent':analyze.get_sentiment(t['text'])}})
 
-    
-    return render(request,'tweet.html',{'content':content})
+        
+        return render(request,'tweet.html',{'content':content})
+    else:
+        return redirect('index')
